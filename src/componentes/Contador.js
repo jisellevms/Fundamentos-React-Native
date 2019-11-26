0f90d7f6f8268af2e1cfc3515787d5402db9b6a0
JIVM <jiselle.santana@gfxconsultoria.com>
2019-11-26 15:57:21 -0300

    Alteração valor inicial do contador

--- src/componentes/Contador.js
+++ src/componentes/Contador.js
@@ -5,14 +5,14 @@ import Padrao from '../estilo/Padrao'
 export default class Contador extends React.Component {
 
     state = {
-        numero: 0
+        numero: 1
     } 
 
     maisUm ()  {
         this.setState({ numero: this.state.numero + 1 })
     }
     limpar = () => { // 1° Forma de fazer arrow fuction
-        this.setState({ numero: 0 })
+        this.setState({ numero: 1 })
     }
     render() {
         return (
