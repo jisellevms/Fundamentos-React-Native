e0d43976a29db17972fbd75131ba0706ce90a727
JIVM <jiselle.santana@gfxconsultoria.com>
2019-11-26 15:34:25 -0300

    Adicionando navegação, cliques, classes, incrementos

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src';
+import App from './src/App.js';
 import {name as appName} from './app.json';
+import Menu from './src/Menu'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Menu);
